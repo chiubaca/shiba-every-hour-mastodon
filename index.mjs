2c720b08c32f4f4ad7dea6f4a3292052fbe89eea
Alex Chiu <alexchiu11@gmail.com>
2023-01-14 17:53:55 +0000

    feat: initial commit, post shibe to mastodon script working

---
+++ index.mjs
@@ -0,0 +1,46 @@
+import { login } from "masto";
+import fetch from "node-fetch";
+
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+async function sendImageToMasto(shibeImageArrayBuffer) {
+  try {
+    const masto = await login({
+      url: "https://mas.to/api/v1",
+      accessToken: process.env.ACCESS_TOKEN_KEY,
+    });
+
+    // convert ArrayBuffer to Buffer
+    const shibeImageBuffer = Buffer.from(shibeImageArrayBuffer);
+
+    const attachment = await masto.v2.mediaAttachments.create({
+      file: shibeImageBuffer,
+      description: "a random shiba",
+    });
+
+    await masto.v1.statuses.create({
+      status: "",
+      visibility: "public",
+      mediaIds: [attachment.id],
+    });
+
+    console.log("🐕‍🦺 New shibe successfully posted! 🐕‍🦺");
+  } catch (error) {
+    console.error(
+      "Thats ruff, something went wrong posting to Mastodon...😒",
+      error
+    );
+  }
+}
+
+fetch("http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true")
+  .then((res) => res.json())
+  .then((json) => json[0])
+  .catch((error) =>
+    console.error("There was an error fetching a shibe...😑", error)
+  )
+  .then((imageURL) => fetch(imageURL))
+  .then((res) => res.arrayBuffer())
+  .then((shibeImageArrayBuffer) => sendImageToMasto(shibeImageArrayBuffer));
